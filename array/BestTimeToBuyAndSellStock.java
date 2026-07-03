9ab1b475179ea51455eedbf52910c6b20ba60499
kishorekh03 <kishorekh018@gmail.com>
2026-07-03 13:40:36 +0530

    BestTimeToBuyAndSellStock is added

---
+++ array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,34 @@
+package array;
+
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+
+        int bestBuy = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+
+            if (prices[i] < bestBuy) {
+                bestBuy = prices[i];
+            }
+
+            int profit = prices[i] - bestBuy;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+        return maxProfit;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] prices = {7, 6,4,3,1};
+
+        BestTimeToBuyAndSellStock obj = new BestTimeToBuyAndSellStock();
+
+        System.out.println("Max Profit:"+obj.maxProfit(prices));
+    }
+}
