7a2d25a856d3c4046e90a69b653d0541b717bcf8
kishorekh03 <kishorekh018@gmail.com>
2026-07-05 12:33:33 +0530

    Search in Rotated Sorted Array is added

--- array/RotatedSortedArray.java
+++ array/RotatedSortedArray.java
@@ -4,38 +4,51 @@ public class RotatedSortedArray {
 
     public int search(int[] nums, int target) {
 
+        // Search space
         int start = 0;
         int end = nums.length - 1;
 
+        // Perform modified binary search
         while (start <= end) {
 
+            // Calculate middle index
             int mid = start + (end - start) / 2;
 
+            // Target found
             if (nums[mid] == target) {
                 return mid;
             }
 
-            // Left half is sorted
+            // Check if the left half is sorted
             if (nums[start] <= nums[mid]) {
 
+                // If target lies within the sorted left half,
+                // discard the right half.
                 if (nums[start] <= target && target <= nums[mid]) {
                     end = mid - 1;
-                } else {
+                }
+                // Otherwise, search in the right half.
+                else {
                     start = mid + 1;
                 }
 
             }
-            // Right half is sorted
+            // Otherwise, the right half must be sorted.
             else {
 
+                // If target lies within the sorted right half,
+                // discard the left half.
                 if (nums[mid] <= target && target <= nums[end]) {
                     start = mid + 1;
-                } else {
+                }
+                // Otherwise, search in the left half.
+                else {
                     end = mid - 1;
                 }
             }
         }
 
+        // Target not present
         return -1;
     }
 
